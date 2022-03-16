cf4a0f2dabe02b211d1cc89279a213ba3b58a7c1
Garv Jain <garvj03@gmail.com>
2022-03-16 22:03:15 +0530

    chore: init

---
+++ app/root.tsx
@@ -0,0 +1,43 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  LinksFunction,
+  Scripts,
+  ScrollRestoration,
+} from "remix";
+import type { MetaFunction } from "remix";
+import styles from "~/styles/app.css";
+
+export const meta: MetaFunction = () => {
+  return { title: "Garv Jain" };
+};
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: styles,
+    },
+  ];
+};
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
