0265a05079021587758a139f85ed64ac55305dbd
Garv Jain <garvj03@gmail.com>
2022-03-17 14:11:56 +0530

    chore: configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,31 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        pulse: {
+          "0%, 100%": {
+            opacity: 1,
+          },
+          "50%": {
+            opacity: 0.75,
+          },
+        },
+      },
+      animation: {
+        pulse: "pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+      },
+      colors: {
+        "primary-dark": "#1E2234",
+        "secondary-dark": "#22263B",
+        "soft-red": "#FF4A57",
+      },
+      fontFamily: {
+        sans: ["Karla", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
