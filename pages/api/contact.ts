cb96eb0aaf7db8edb680c3a7c1181a88dd805763
GarvJain03 <garvj03@gmail.com>
2022-04-10 17:38:19 +0530

    chore: vercel does not support
    Promises are not supported in vercel's serverless functions.

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,7 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import invariant from 'tiny-invariant'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const { name, email, message } = req.body
 
   if (!(name && email && message)) {
@@ -13,27 +16,27 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     'DISCORD_WEBHOOK_URL is not defined'
   )
 
-  fetch(process.env.DISCORD_WEBHOOK_URL!, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      embeds: [
-        {
-          title: name,
-          description: message,
-          footer: {
-            text: email,
+  try {
+    await fetch(process.env.DISCORD_WEBHOOK_URL!, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        embeds: [
+          {
+            title: name,
+            description: message,
+            footer: {
+              text: email,
+            },
           },
-        },
-      ],
-    }),
-  })
-    .then(() => {
-      res.status(200).json({ msg: 'Success' })
+        ],
+      }),
     })
-    .catch(() => {
-      res.status(500).json({ msg: 'Internal Server Error' })
-    })
+    return res.status(200).json({ msg: 'Success' })
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json({ msg: 'Internal Server Error' })
+  }
 }
