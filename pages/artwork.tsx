a15a62b394676291ec602889e3abbf46c4fc6b12
GarvJain03 <garvj03@gmail.com>
2022-04-15 01:05:11 +0530

    wip artowork page

---
+++ pages/artwork.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ComingSoon from '../components/ComingSoon'
+import Layout from '../components/Layout/Layout'
+
+const ArtPage = () => {
+  return (
+    <Layout title='Artwork'><ComingSoon/></Layout>
+  )
+}
+
+export default ArtPage
